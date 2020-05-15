45bbad36277867b10036e352e7621801c093c0bf
takuro <takutaku.handa27@gmail.com>
2020-05-15 16:57:24 +0900

    ikkaimeno

---
+++ myproject/accounts/views.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
+
+from . import forms
+
+
+class MyLoginView(LoginView):
+    form_class = forms.LoginForm
+    template_name = "accounts/login.html"
+
+
+class MyLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = "accounts/logout.html"
+
+
+class IndexView(TemplateView):
+    template_name = "accounts/index.html"
+
+
+class UserCreateView(CreateView):
+    form_class = UserCreationForm
+    template_name = "accounts/create.html"
+    success_url = reverse_lazy("login")
---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\半田拓朗\desktop\django\django5\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
